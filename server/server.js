8edbc36a33e5b0ede3a024878ff14c3e9005e8ac
nguyent0275 <100709940+nguyent0275@users.noreply.github.com>
2024-03-11 21:06:37 -0400

    deploying

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
 
-// mongodb password
+// mongodb password s
 // qZ2EgBF4tqVyWr7P
 
 const { typeDefs, resolvers } = require("./schemas");
