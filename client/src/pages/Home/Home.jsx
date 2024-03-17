7b6177a485220178f76c7819132f61a9045540d1
nguyent0275 <100709940+nguyent0275@users.noreply.github.com>
2024-03-17 00:51:27 -0400

    working on a lifetime search of spending based on all budgets

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,41 +7,61 @@ import "./home.css";
 import Auth from "../../utils/auth";
 import "reactjs-popup/dist/index.css";
 import { useQuery } from "@apollo/client";
+import { useState } from "react";
 import { QUERY_ME } from "../../utils/queries";
-import CurrentBudget from "../../components/CurrentBudget/CurrentBudget"
+import CurrentBudget from "../../components/CurrentBudget/CurrentBudget";
+import AllBudgetSpending from "../../components/AllBudgetSpending"
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ME);
   const user = data?.me || [];
-  
+
+  const [showCurrent, setShowCurrent] = useState(true);
+
+  const handleSwitch = () => {
+    setShowCurrent(!showCurrent);
+  };
+
   if (loading) {
     return <div>Loading... </div>;
   }
 
   if (Auth.loggedIn()) {
-    return (
-      <>
-        <main>
-          <div>
-            <Popup
-              trigger={<button> Add Budget </button>}
-              position="right center"
-              modal
-            >
-              {(close) => (
-                <div className="modal-container">
-                  <div className="modal-content">
-                    <BudgetForm userId={user._id} />
-                    <button onClick={() => close()}>Close Modal</button>
+    if (showCurrent) {
+      return (
+        <>
+          <main>
+            <div>
+              <Popup
+                trigger={<button> Add Budget </button>}
+                position="right center"
+                modal
+              >
+                {(close) => (
+                  <div className="modal-container">
+                    <div className="modal-content">
+                      <BudgetForm userId={user._id} />
+                      <button onClick={() => close()}>Close Modal</button>
+                    </div>
                   </div>
-                </div>
-              )}
-            </Popup>
-          </div>
-          <CurrentBudget user={user} />
-        </main>
+                )}
+              </Popup>
+              <button onClick={handleSwitch}>Show Budget Spending</button>
+            </div>
+            <CurrentBudget user={user} />
+          </main>
+        </>
+      );
+    }
+    return(
+      <>
+      <div>
+        All Budget Spending
+      </div>
+      <button onClick={handleSwitch}>Show Current Budget</button>
+      <AllBudgetSpending />
       </>
-    );
+    )
   }
   return (
     <>
