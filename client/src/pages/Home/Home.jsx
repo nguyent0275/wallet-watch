9f68ecfc7d3d099ff8b3d15de05989c02a21ddab
Toan <nguyentoan0275@gmail.com>
2024-03-17 11:53:34 -0400

    added proper homepage serving based on condtions with logged in user having or not having a current monthly budget

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,11 +10,14 @@ import { useQuery } from "@apollo/client";
 import { useState } from "react";
 import { QUERY_ME } from "../../utils/queries";
 import CurrentBudget from "../../components/CurrentBudget/CurrentBudget";
-import AllBudgetSpending from "../../components/AllBudgetSpending"
+// import AllBudgetSpending from "../../components/AllBudgetSpending";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_ME);
   const user = data?.me || [];
+  // console.log(user);
+  const budgets = user.budgets;
+  console.log(budgets);
 
   const [showCurrent, setShowCurrent] = useState(true);
 
@@ -22,11 +25,42 @@ const Home = () => {
     setShowCurrent(!showCurrent);
   };
 
+  // conditional rendering
+  // if data is still being retrieved
   if (loading) {
     return <div>Loading... </div>;
   }
 
-  if (Auth.loggedIn()) {
+  // if user is logged in and has no budget
+  if ((Auth.loggedIn() && budgets.length === 0) || undefined) {
+    if (showCurrent) {
+      return (
+        <>
+          <main>
+            <div>
+              <Popup
+                trigger={<button> Add Budget </button>}
+                position="right center"
+                modal
+              >
+                {(close) => (
+                  <div className="modal-container">
+                    <div className="modal-content">
+                      <BudgetForm userId={user._id} />
+                      <button onClick={() => close()}>Close Modal</button>
+                    </div>
+                  </div>
+                )}
+              </Popup>
+              <button onClick={handleSwitch}>Show Budget Spending</button>
+            </div>
+          </main>
+        </>
+      );
+    }
+  }
+  // if user is loggedin and has a current month budget
+  if (Auth.loggedIn && budgets.length > 0) {
     if (showCurrent) {
       return (
         <>
@@ -53,16 +87,15 @@ const Home = () => {
         </>
       );
     }
-    return(
-      <>
-      <div>
-        All Budget Spending
-      </div>
-      <button onClick={handleSwitch}>Show Current Budget</button>
-      <AllBudgetSpending />
-      </>
-    )
   }
+
+  // return (
+  //   <>
+  //     <div>All Budget Spending</div>
+  //     <button onClick={handleSwitch}>Show Current Budget</button>
+  //     <AllBudgetSpending />
+  //   </>
+  // );
   return (
     <>
       <div id="card-container">
