3f84450a68679fb84b93cd133de06cc3a5f2d58a
Toan <nguyentoan0275@gmail.com>
2024-03-17 14:40:07 -0400

    duplicate budget will now serve the correct error message in the modal

--- client/src/components/BudgetForm/BudgetForm.jsx
+++ client/src/components/BudgetForm/BudgetForm.jsx
@@ -1,4 +1,5 @@
 import { useMutation } from "@apollo/client";
+import { useState } from "react";
 import { ADD_BUDGET } from "../../utils/mutations";
 import MonthOptions from "../Dropdowns/MonthOptions";
 import YearOptions from "../Dropdowns/YearOptions";
@@ -22,7 +23,11 @@ const BudgetForm = ({ user }) => {
   console.log(budgetMonthAndYearsArray);
 
   // addBudget is given the ADD_BUDGET mutation functionality
+  // erorr is for backend errors
   const [addBudget, { error }] = useMutation(ADD_BUDGET);
+
+  // useState variable for serving front end error
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleFormSubmit = async (event) => {
     // add event as an arg for handleFormSubmit as well
@@ -37,16 +42,17 @@ const BudgetForm = ({ user }) => {
       let addedBudget = budgetMonth + " " + String(budgetYear);
       // checks it user input vs. budgetMonthAndYearsArray (budgets they already have)
       // prevent duplicates from being added to the user's budget list
+      // serves front end error message with state variable
       for (let i = 0; i < budgetMonthAndYearsArray.length; i++) {
         if (addedBudget === budgetMonthAndYearsArray[i]) {
-          console.log("This month already exists");
+          setErrorMessage("There is already a budget for this month and year");
           return;
         }
       }
-      const { data } = await addBudget({
+      // runs the mutation with user input
+      await addBudget({
         variables: { userId, budgetMonth, budgetYear },
       });
-      console.log(data);
       // if user is adding a budget from the homepage, it will redirect them to their profile page and show that new budget
       window.location.replace("/user/");
     } catch (err) {
@@ -65,7 +71,10 @@ const BudgetForm = ({ user }) => {
           <YearOptions />
         </select>
         <button type="submit">Submit</button>
-        {error && <div>This month already beongs</div>}
+        {errorMessage && <div id="errorMessage">{errorMessage}</div>}
+        {error && (
+          <div>Something went horrible wrong! Please contact support.</div>
+        )}
       </form>
     </>
   );
