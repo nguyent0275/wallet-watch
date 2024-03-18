0aa4d0f7641424b06b6298eeb45508ae48e9cd62
savannahjk02 <savannahjk02@gmail.com>
2024-03-17 21:01:11 -0400

    get started button

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 //React Imports
-import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import {Link} from "react-router-dom";
 
 //CSS Imports
 import "./card.css";
@@ -25,11 +25,13 @@ const InfoCard = () => {
       <Card className="card">
         <Card.Body>
           <Card.Title>Take charge of your finances</Card.Title>
-          <Card.Text className=".card-text">{cardParagraph1}</Card.Text>
-          <Card.Text className=".card-text">{cardParagraph2}</Card.Text>
-          <Link to="/signup">
-          <Button variant="dark">Get Started!</Button>
-          </Link>
+          <Card.Text class=".card-text">
+          Saving money in today's world is tough. Monthly subscriptions and memberships can quickly add up, leaving you stressed about your finances. With Wallet Watch, we make it easy. Our app lets you effortlessly track all your daily expenses, ensuring you're always in control of your finances.
+          </Card.Text>
+          <Card.Text class=".card-text">
+          Wallet Watch allows you to set up monthly budgets tailored to your needs. Whether you're saving for a vacation or simply want to cut back on expenses, Wallet Watch helps you stay on track. Say hello to stress-free budgeting!
+          </Card.Text>
+          <Link to="/signup"><Button variant="dark">Get Started!</Button></Link>
         </Card.Body>
       </Card>
     </div>
